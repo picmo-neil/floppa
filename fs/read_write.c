02077126634e057a13575424ea663808cdd026c1
Flopster101 <nahuelgomez329@gmail.com>
2026-01-15 10:24:53 -0300

    fs: Disable sys_read hook for SukiSU Ultra
    According to https://github.com/backslashxx/KernelSU/issues/5#issuecomment-2676829117,
    it is now deprecated.
    
    Kernel fails to build with:
    
    ld.lld: error: undefined symbol: ksu_vfs_read_hook
    >>> referenced by win_minmax.c
    >>>               vmlinux.o:(sys_read)
    >>> referenced by win_minmax.c
    >>>               vmlinux.o:(sys_read)
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- fs/read_write.c
+++ fs/read_write.c
@@ -574,7 +574,7 @@ extern int ksu_handle_sys_read(unsigned int fd, char __user **buf_ptr,
 			size_t *count_ptr);
 #endif
 
-#ifdef CONFIG_KSU_SUSFS
+#if defined(CONFIG_KSU_SUSFS) && !defined(CONFIG_KSU_SUKI)
 extern bool ksu_vfs_read_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,
 			char __user **buf_ptr, size_t *count_ptr);
@@ -589,7 +589,7 @@ SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)
 	if (unlikely(ksu_vfs_read_hook)) 
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
-#ifdef CONFIG_KSU_SUSFS
+#if defined(CONFIG_KSU_SUSFS) && !defined(CONFIG_KSU_SUKI)
 	if (unlikely(ksu_vfs_read_hook))
 		ksu_handle_sys_read(fd, &buf, &count);
 #endif
