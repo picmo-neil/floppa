806bc9d9a88e62649b2d416bdb717c0f1b58b745
picmo-neil <farhanlabibswapnil@gmail.com>
2026-01-27 21:28:11 +0600

    fix typo

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -574,7 +574,7 @@ struct cfs_rq {
 	 */
 	struct sched_avg avg;
 	unsigned long runnable_sum;
-	unsigned long runnable_avg
+	unsigned long runnable_avg;
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	unsigned long tg_load_avg_contrib;
 	long propagate;
